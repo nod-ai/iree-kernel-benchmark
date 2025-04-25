fd477445b6b62e0a6252e710b887713a1f27a6a4
Andrea Faulds <andrea.faulds@amd.com>
2025-04-25 18:31:41 +0200

    gemmbench: Add support for dynamic dims, use for llama8b_prefill
    Dynamic dims seem to be appropriate for llama8b_prefill based on samples
    from https://github.com/nod-ai/playbook/issues/63

--- tests/test_gemmbench_mlir_gen.py
+++ tests/test_gemmbench_mlir_gen.py
@@ -1,4 +1,8 @@
-from iree_kernel_benchmark.gemmbench.gemm_utils import GemmConfig, generate_mlir
+from iree_kernel_benchmark.gemmbench.gemm_utils import (
+    GemmConfig,
+    generate_mlir,
+    kDynamic,
+)
 from .utils import match_lines
 from iree.compiler import ir
 import pytest
@@ -65,6 +69,130 @@ def test_n_t_f8_f32_f8():
             "%2 = linalg.matmul_transpose_b {cast = #linalg.type_fn<cast_signed>} ins(%arg0, %arg1 : tensor<512x14336xf8E4M3FNUZ>, tensor<4096x14336xf8E4M3FNUZ>) outs(%1 : tensor<512x4096xf32>) -> tensor<512x4096xf32>",
             "%3 = arith.truncf %2 : tensor<512x4096xf32> to tensor<512x4096xf8E4M3FNUZ>",
             "return %3 : tensor<512x4096xf8E4M3FNUZ>",
+        ],
+    )
+
+
+def test_n_t_f16_f32_f16_dynamic_dim_M():
+    # From 'llama8b_prefill'
+    cfg = GemmConfig(
+        M=kDynamic,
+        N=14336,
+        K=4096,
+        tA="N",
+        tB="T",
+        operand_element_type="f16",
+        accumulator_element_type="f32",
+        result_element_type="f16",
+        runtime_dim=512,  # Unused, included for correctness
+    )
+    mlir = generate_mlir(cfg)
+    match_lines(
+        mlir,
+        [
+            "module {",
+            "func.func @main(%arg0: tensor<?x4096xf16>, %arg1: tensor<14336x4096xf16>) -> tensor<?x14336xf16> {",
+            "%cst = arith.constant 0.000000e+00 : f32",
+            "%c0 = arith.constant 0 : index",
+            "%dim = tensor.dim %arg0, %c0 : tensor<?x4096xf16>",
+            "%0 = tensor.empty(%dim) : tensor<?x14336xf32>",
+            "%1 = linalg.fill ins(%cst : f32) outs(%0 : tensor<?x14336xf32>) -> tensor<?x14336xf32>",
+            "%2 = linalg.matmul_transpose_b {cast = #linalg.type_fn<cast_signed>} ins(%arg0, %arg1 : tensor<?x4096xf16>, tensor<14336x4096xf16>) outs(%1 : tensor<?x14336xf32>) -> tensor<?x14336xf32>",
+            "%3 = arith.truncf %2 : tensor<?x14336xf32> to tensor<?x14336xf16>",
+            "return %3 : tensor<?x14336xf16>",
+        ],
+    )
+
+
+def test_t_n_f16_f32_f16_dynamic_dim_N():
+    # Synthetic example (modified from test_n_t_f16_f32_f16_dynamic_dim_M)
+    cfg = GemmConfig(
+        M=512,
+        N=kDynamic,
+        K=4096,
+        tA="T",
+        tB="N",
+        operand_element_type="f16",
+        accumulator_element_type="f32",
+        result_element_type="f16",
+        runtime_dim=14366,  # Unused, included for correctness
+    )
+    mlir = generate_mlir(cfg)
+    match_lines(
+        mlir,
+        [
+            "module {",
+            "func.func @main(%arg0: tensor<4096x512xf16>, %arg1: tensor<4096x?xf16>) -> tensor<512x?xf16> {",
+            "%cst = arith.constant 0.000000e+00 : f32",
+            "%c1 = arith.constant 1 : index",
+            "%dim = tensor.dim %arg1, %c1 : tensor<4096x?xf16>",
+            "%0 = tensor.empty(%dim) : tensor<512x?xf32>",
+            "%1 = linalg.fill ins(%cst : f32) outs(%0 : tensor<512x?xf32>) -> tensor<512x?xf32>",
+            "%2 = linalg.matmul_transpose_a {cast = #linalg.type_fn<cast_signed>} ins(%arg0, %arg1 : tensor<4096x512xf16>, tensor<4096x?xf16>) outs(%1 : tensor<512x?xf32>) -> tensor<512x?xf32>",
+            "%3 = arith.truncf %2 : tensor<512x?xf32> to tensor<512x?xf16>",
+            "return %3 : tensor<512x?xf16>",
+        ],
+    )
+
+
+def test_n_n_f16_f32_f16_dynamic_dim_K():
+    # Synthetic example (modified from test_n_t_f16_f32_f16_dynamic_dim_M)
+    cfg = GemmConfig(
+        M=512,
+        N=14366,
+        K=kDynamic,
+        tA="N",
+        tB="N",
+        operand_element_type="f16",
+        accumulator_element_type="f32",
+        result_element_type="f16",
+        runtime_dim=4096,  # Unused, included for correctness
+    )
+    mlir = generate_mlir(cfg)
+    match_lines(
+        mlir,
+        [
+            "module {",
+            "func.func @main(%arg0: tensor<512x?xf16>, %arg1: tensor<?x14366xf16>) -> tensor<512x14366xf16> {",
+            "%cst = arith.constant 0.000000e+00 : f32",
+            "%0 = tensor.empty() : tensor<512x14366xf32>",
+            "%1 = linalg.fill ins(%cst : f32) outs(%0 : tensor<512x14366xf32>) -> tensor<512x14366xf32>",
+            "%2 = linalg.matmul ins(%arg0, %arg1 : tensor<512x?xf16>, tensor<?x14366xf16>) outs(%1 : tensor<512x14366xf32>) -> tensor<512x14366xf32>",
+            "%3 = arith.truncf %2 : tensor<512x14366xf32> to tensor<512x14366xf16>",
+            "return %3 : tensor<512x14366xf16>",
+        ],
+    )
+
+
+def test_n_n_f16_f32_f16_dynamic_dim_M_N():
+    # Synthetic example (modified from test_n_t_f16_f32_f16_dynamic_dim_M)
+    cfg = GemmConfig(
+        M=kDynamic,
+        N=kDynamic,
+        K=4096,
+        tA="N",
+        tB="N",
+        operand_element_type="f16",
+        accumulator_element_type="f32",
+        result_element_type="f16",
+        runtime_dim=512,  # Unused, included for correctness
+    )
+    mlir = generate_mlir(cfg)
+    match_lines(
+        mlir,
+        [
+            "module {",
+            "func.func @main(%arg0: tensor<?x4096xf16>, %arg1: tensor<4096x?xf16>) -> tensor<?x?xf16> {",
+            "%cst = arith.constant 0.000000e+00 : f32",
+            "%c0 = arith.constant 0 : index",
+            "%dim = tensor.dim %arg0, %c0 : tensor<?x4096xf16>",
+            "%c1 = arith.constant 1 : index",
+            "%dim_0 = tensor.dim %arg1, %c1 : tensor<4096x?xf16>",
+            "%0 = tensor.empty(%dim, %dim_0) : tensor<?x?xf32>",
+            "%1 = linalg.fill ins(%cst : f32) outs(%0 : tensor<?x?xf32>) -> tensor<?x?xf32>",
+            "%2 = linalg.matmul ins(%arg0, %arg1 : tensor<?x4096xf16>, tensor<4096x?xf16>) outs(%1 : tensor<?x?xf32>) -> tensor<?x?xf32>",
+            "%3 = arith.truncf %2 : tensor<?x?xf32> to tensor<?x?xf16>",
+            "return %3 : tensor<?x?xf16>",
         ],
     )
 
