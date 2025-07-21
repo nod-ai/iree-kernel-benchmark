19bcc77e7c8fba83ff8c91894c995f18d6acaddd
Fabian Mora <fabian.mora-cordero@amd.com>
2025-07-21 16:36:30 +0000

    address reviewer comments

--- turbine-ukernels/bench_attention.sh
+++ turbine-ukernels/bench_attention.sh
@@ -2,12 +2,9 @@
 
 set -euxo pipefail
 
-for type in "bf16" "f16";
-do
-  for s0 in 128 137 4096 4099;
-  do
-    for gqa in 1;
-    do
+for type in "bf16" "f16"; do
+  for s0 in 128 137 4096 4099; do
+    for gqa in 1; do
       [[ $gqa = 1 ]] && enable_gqa="" || enable_gqa="--gqa"
       python attention.py -c gfx942 -b 4 -m ${s0} -n 128 -k1 128 -k2 ${s0} --dynamic-dims m k2 --dtype ${type} ${enable_gqa}
     done
