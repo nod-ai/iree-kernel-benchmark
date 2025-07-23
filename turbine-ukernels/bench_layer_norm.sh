ed35eef89f6702eb4a3683f541905060d1653e3b
Fabian Mora <fabian.mora-cordero@amd.com>
2025-07-23 19:57:05 +0000

    add more test shapes

--- turbine-ukernels/bench_layer_norm.sh
+++ turbine-ukernels/bench_layer_norm.sh
@@ -4,6 +4,9 @@ set -euxo pipefail
 
 configs=(
   "-t f32 -b 32 2048 -n 256 -a --bias --eps 1e-6"
+  "-t f32 -b 32 1 2048 -n 256 -a --bias -p 0 2 1 3 --eps 1e-6"
+  "-t f16 -b 32 1 2048 -n 256 -a --bias -p 0 2 1 3 --eps 1e-6"
+  "-t bf16 -b 32 1 16 -n 2048 -a --bias -p 0 2 3 1 --eps 1e-6"
 )
 
 for conf in "${configs[@]}"; do
